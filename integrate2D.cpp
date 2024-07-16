f7fade1a434b0939dbe6663033b47d2db2130751
365614269 <365614269@qq.com>
2024-07-16 09:30:49 +0800

    Updated abscissae.h and weights.h

--- integrate2D.cpp
+++ integrate2D.cpp
@@ -1,35 +1,26 @@
 #include <iostream>
 #include <fstream>
 #include "integrate2D.h"
+#include "abscissae.h"
+#include "weights.h"
 
 using namespace std;
 
 #define PRECISION 64
 
 long double integral(long double (Node::*ref)(long double, long double, int, int), int alpha, int beta, long double a1, long double b1, long double a2, long double b2, Node& node) {
-    ifstream weights("weights.txt");
-    ifstream abscissae("abscissae.txt");
     long double diff1 = (b1 - a1) / 2;
     long double avg1 = (b1 + a1) / 2;
     long double diff2 = (b2 - a2) / 2;
     long double avg2 = (b2 + a2) / 2;
 
     long double ans = 0;
-    long double w[PRECISION], x[PRECISION];
-
-    for (int i = 0; i < PRECISION; i++) {
-        weights >> w[i];
-        abscissae >> x[i];
-    }
 
     for (int i = 0; i < PRECISION; i++) {
         for (int j = 0; j < PRECISION; j++) {
-            ans += w[i] * w[j] * (node.*ref)(diff1 * x[i] + avg1, diff2 * x[j] + avg2, alpha, beta);
+            ans += weights[i] * weights[j] * (node.*ref)(diff1 * abscissae[i] + avg1, diff2 * abscissae[j] + avg2, alpha, beta);
         }
     }
-
-    weights.close();
-    abscissae.close();
 
     return diff1 * diff2 * ans;
 }
